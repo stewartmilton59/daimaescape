d4a79d2a4d16e2c9edb721af9f4086cd714eee02
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-01 01:48:02 +0300

    Update settings.py

--- daimaescape/settings.py
+++ daimaescape/settings.py
@@ -92,11 +92,20 @@ WSGI_APPLICATION = 'daimaescape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Postgres DB_CONFGARATIONS
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://postgres:QMXsxhlDKnLHrgDiLemfYBLabQqyRoia@shortline.proxy.rlwy.net:25580/railway'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
