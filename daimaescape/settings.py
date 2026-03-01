18493f5803f824f26c7dbe7671c7576a98765d51
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-01 06:42:11 +0000

    main

--- daimaescape/settings.py
+++ daimaescape/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from django.utils.translation import gettext_lazy as _
 from django.templatetags.static import static
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,11 +93,19 @@ WSGI_APPLICATION = 'daimaescape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('postgresql://postgres:QMXsxhlDKnLHrgDiLemfYBLabQqyRoia@shortline.proxy.rlwy.net:25580/railway'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Postgres DB_CONFGARATIONS
