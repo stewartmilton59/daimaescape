4f0fd424f626a8490b67e87a3786e632d7bd5055
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-01 14:49:38 +0300

    Update settings.py

--- daimaescape/settings.py
+++ daimaescape/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from django.utils.translation import gettext_lazy as _
 from django.templatetags.static import static
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -93,30 +93,20 @@ WSGI_APPLICATION = 'daimaescape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-# Postgres DB_CONFGARATIONS
 # DATABASES = {
 #     'default': dj_database_url.config(
-#         default=os.environ.get('postgresql://postgres:QMXsxhlDKnLHrgDiLemfYBLabQqyRoia@postgres.railway.internal:5432/railway'),
+#         default=os.environ.get('DATABASE_URL'),
 #         conn_max_age=600,
-#         conn_health_checks=True,
+#         ssl_require=True
 #     )
 # }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
