8a9d84ea93d7d2c169beb324913176d1ef42bdc5
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-01 22:31:58 +0000

    main

--- daimaescape/settings.py
+++ daimaescape/settings.py
@@ -100,13 +100,18 @@ WSGI_APPLICATION = 'daimaescape.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
-
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    # Fallback to SQLite for local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
