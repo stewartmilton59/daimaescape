67ffa6156e18a4a9ae10741814f6d8bb07ef30c5
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-03 00:26:07 +0300

    main

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('team/', views.team, name='team'),
     path('service/', views.service, name='service'),
     path('testimonial/', views.testimonial, name='testimonial'),
     path('house-rules/', views.house_rules, name='house_rules'),
--- core/views.py
+++ core/views.py
@@ -3,40 +3,7 @@ from rooms.models import Room, HouseRule, LaundryService
 
 
 def home(request):
-    # Get featured villas that are available
-    featured_villas = Room.objects.filter(
-        is_available=True, is_featured=True)[:3]
-
-    # If no featured villas, get any 3 available villas
-    if not featured_villas:
-        featured_villas = Room.objects.filter(is_available=True)[:3]
-
-    # Get house rules for display (optional)
-    house_rules = HouseRule.objects.all()[:6]
-
-    # Get laundry services
-    laundry_services = LaundryService.objects.all()
-
-    context = {
-        # Keep same variable name for template compatibility
-        'featured_rooms': featured_villas,
-        'featured_villas': featured_villas,
-        'house_rules': house_rules,
-        'laundry_services': laundry_services,
-    }
-
-    # Get featured rooms that are available, limit to 3 for home page
-    featured_rooms = Room.objects.filter(
-        is_available=True, is_featured=True)[:3]
-
-    # If no featured rooms, get any 3 available rooms
-    if not featured_rooms:
-        featured_rooms = Room.objects.filter(is_available=True)[:3]
-
-    context = {
-        'featured_rooms': featured_rooms,
-    }
-    return render(request, 'core/home.html', context)
+    return render(request, 'core/home.html')
 
 
 def about(request):
@@ -49,10 +16,6 @@ def contact(request):
 
 def service(request):
     return render(request, 'core/service.html')
-
-
-def team(request):
-    return render(request, 'core/team.html')
 
 
 def testimonial(request):
