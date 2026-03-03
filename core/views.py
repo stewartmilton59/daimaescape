117e572f2ecc11c7d2fb9684460376adc8d45223
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-03 00:28:36 +0000

    main

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from rooms.models import Room, HouseRule, LaundryService
 
 
 def home(request):
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
-from django.db.models import Q
-from .models import Room
+from django.shortcuts import render
 
 
 def room_list(request):
-    return render(request, 'rooms/room_detail.html', context)
+    return render(request, 'rooms/room_detail.html')
+
+def room_detail(request):
+    return render(request, 'rooms/room_detail.html')
