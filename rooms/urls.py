f0dc252937a9402106c412afb23912ad65a230eb
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-03 08:17:37 +0000

    main

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'rooms'
 
 urlpatterns = [
+    path('', views.room_list, name='room_list'),
     path('two-bedroom-villa/', views.two_bedroom_villa, name='two-bedroom-villa'),
     path('one-bedroom-villa/', views.one_bedroom_villa, name='one-bedroom-villa'),
-    path('', views.room_detail, name='room_list'),
 ]
