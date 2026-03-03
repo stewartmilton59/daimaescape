5d0a9af580418b8d8dd51fa97e54959310da5c0a
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-03 18:22:00 +0300

    main

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('service/', views.service, name='service'),
-    path('testimonial/', views.testimonial, name='testimonial'),
     path('house-rules/', views.house_rules, name='house_rules'),
     path('menu/', views.menu, name='menu'),
     path('attractions/', views.attractions, name='attractions'),
--- core/views.py
+++ core/views.py
@@ -17,22 +17,13 @@ def service(request):
     return render(request, 'core/service.html')
 
 
-def testimonial(request):
-    return render(request, 'core/testimonial.html')
-
-
 def house_rules(request):
-    """Display all house rules"""
-    from rooms.models import HouseRule
-    rules = HouseRule.objects.all()
     return render(request, 'core/house_rules.html', {'rules': rules})
 
 
 def menu(request):
-    """Display restaurant menu"""
     return render(request, 'core/menu.html')
 
 
 def attractions(request):
-    """Display local attractions"""
     return render(request, 'core/attractions.html')
