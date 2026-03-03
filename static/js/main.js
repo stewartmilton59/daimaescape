9614acb5fbd4bf3ecc989b3dfec41ca6707beff6
Stewart Milton <136900060+stewartmilton59@users.noreply.github.com>
2026-03-03 18:02:26 +0300

    main

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,3 @@
-(function ($) {
-    "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
     // Initiate the wowjs
     new WOW().init();
     
